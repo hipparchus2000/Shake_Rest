6ba09e4e3ea4617e3813e3fccb464393ec87fe9b
Jeff Davies <hipparchus2000@gmail.com>
2017-08-02 19:00:56 +0100

    additional protection bank endpoints

--- api/models/bankModel.js
+++ api/models/bankModel.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let BankSchema = new Schema({
-	
+	transactionDate: {
+		type: String,
+	},
+	notes: {
+		type: String,
+	},
+	userId: {
+		type: String
+	},
+	categoryId: {
+		type: String
+	}
 });
 module.exports = mongoose.model('Banks', BankSchema);
 
