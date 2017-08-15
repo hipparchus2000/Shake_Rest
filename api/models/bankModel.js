105d47b824812b4ffa3d4a7c4cb44a575bc69404
Jeff Davies <hipparchus2000@gmail.com>
2017-08-15 18:33:38 +0100

    added bank transaction schema

--- api/models/bankModel.js
+++ api/models/bankModel.js
@@ -6,7 +6,7 @@ let BankSchema = new Schema({
 	Notes: { type: String },
 	userId: { type: String },
 	CategoryId: { type: String },
-	Id { type: String },
+	id { type: String },
 	amount { type: Number },
     SubCategory { type: String },
     AccountId { type: String },
