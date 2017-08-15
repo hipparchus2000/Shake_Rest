ff6a89ad1b7f64b129014ac9f0bae43a75cff3b0
Jeff Davies <hipparchus2000@gmail.com>
2017-08-15 18:24:41 +0100

    added bank transaction schema

--- api/models/bankModel.js
+++ api/models/bankModel.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let BankSchema = new Schema({
-	transactionDate: {
-		type: String,
-	},
-	notes: {
-		type: String,
-	},
-	userId: {
-		type: String
-	},
-	categoryId: {
-		type: String
-	}
+	
+	transactionDate: { type: Date },
+	Notes: { type: String },
+	userId: { type: String },
+	CategoryId: { type: String },
+	Id { type: String },
+	amount { type: Number },
+    SubCategory { type: String },
+    AccountId { type: String },
+    SHA256 { type: String },
+    ManualCategory { type: Boolean }
+	
 });
 module.exports = mongoose.model('Banks', BankSchema);
 
