9b4499b57fe371c2fad1fe11eaa21187188ae089
Jeff Davies <hipparchus2000@gmail.com>
2017-11-29 20:50:05 +0000

    Dev merge fixed bank controller to dev from master

--- api/bank/bankcategoryModel.js
+++ api/bank/bankcategoryModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let BankcategorySchema = new Schema({
+	
 	Id: { type: String },
-	Name: { type: String },
-	SearchString: { type: String },
 	userId: { type: String },
-});
+	Name: { type: String },
+	SearchString: { type: String }
+
+	});
 module.exports = mongoose.model('Bankcategorys', BankcategorySchema);
 
